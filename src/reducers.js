6435c111a8abb1e8ec69574a3e93ef88312edb71
Bibi Khadiza <bibi_khadiza_accenture@whirlpool.com>
2018-03-20 14:35:45 -0400

    Completed combine reducers lab

---
+++ src/reducers.js
@@ -0,0 +1,27 @@
+import { combineReducers } from 'redux'
+
+export const books = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_BOOK":
+      return [].concat(state, action.payload)
+    case "REMOVE_BOOK":
+      let idx = state.indexOf(action.payload)
+      return [].concat(state.slice(0, idx), state.slice(idx + 1, state.length))
+    default:
+      return state
+  }
+}
+
+export const recommendedBooks = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_RECOMMENDED_BOOK":
+      return [].concat(state, action.payload)
+    case "REMOVE_RECOMMENDED_BOOK":
+      let idx = state.indexOf(action.payload)
+      return [].concat(state.slice(0, idx), state.slice(idx + 1, state.length))
+    default:
+      return state
+  }
+}
+
+export const rootReducer = combineReducers({books, recommendedBooks})
